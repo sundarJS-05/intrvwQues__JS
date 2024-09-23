5015c070e21322da9eb7bac1d4be98636869574c
sundarJS-05 <sundar00505@gmail.com>
2024-09-23 17:15:21 +0530

    02

---
+++ 2_#tagGeneration.js
@@ -0,0 +1,42 @@
+/* Challenge - generate # tag from a string
+
+Process
+----------- */
+
+let generateHash = (str)=>{
+
+    if(str.length > 280 || str.trim().length == 0 ){
+        return false
+    }
+
+
+        // convert to array
+        arrayFormat = str.split(" ")
+        // console.log(arrayFormat, '\n', typeof arrayFormat )
+
+        // this will return the ans; but have to do it for each string
+        // console.log(arrayFormat[3].toUpperCase())
+
+        // we can use either forEach or map to iterate;
+        // since forEach doesnt return a value, while map does; we use map
+
+        let outputInter = arrayFormat.map(  (currentElem)=>{
+
+            // returns the 1st letter of each word
+            // console.log(currentElem[0])
+
+            // make 1st letter of each word captial
+            return  currentElem.replace( currentElem[0], currentElem[0].toUpperCase() ) 
+    
+        })
+        // console.log(outputInter)
+
+        console.log("#tag is...............")
+        outputFinal = `#${outputInter.join("")}`
+        return outputFinal
+
+
+}
+
+
+console.log( generateHash("these words form a good hashtag") )
